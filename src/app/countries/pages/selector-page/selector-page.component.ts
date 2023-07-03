da9a6fd9570d0708bfc9ec4960647f33e9c477a7
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-02 22:35:05 -0600

    region Selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -16,8 +18,10 @@ export class SelectorPageComponent {
     borders: ['', [Validators.required]]
   })
 
-  constructor( private fb : FormBuilder ) { }
+  constructor( private fb : FormBuilder, private countriesService:CountriesService ) { }
 
-
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private _region: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania ];
+
   constructor() { }
+
+  get regions(): Region[] {
+    return [...this._region];
+  }
 }
