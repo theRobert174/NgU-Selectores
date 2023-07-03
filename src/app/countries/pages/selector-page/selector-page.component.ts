e3be43564887bdd9488c22443a1efa9814396a92
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-03 06:44:26 -0600

    get countries by region

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -29,8 +30,10 @@ export class SelectorPageComponent implements OnInit{
   }
 
   onRegionChange(): void {
-    this.myForm.get('region')!.valueChanges.subscribe( region => {
-      console.log({region})
+    this.myForm.get('region')!.valueChanges.pipe(
+      switchMap( region => this.countriesService.getCountriesByRegion(region) )
+    ).subscribe( countries => {
+      console.log({countries})
     })
   }
 
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _region: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania ];
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get regions(): Region[] {
     return [...this._region];
   }
 
-  getCountriesByRegion( region: Region) : SmallCountry[] {
-    return [];
+  getCountriesByRegion( region: Region) : Observable<SmallCountry[]> {
+
+    if( !region ) return of([])
+
+    const url : string = `${this.baseUrl}/region/${region}?=fields=cca3,name,borders`;
+
+    return this.http.get<SmallCountry[]>(url).pipe( tap( r => console.log(r) ) );
   }
 }
